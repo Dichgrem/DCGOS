8f85b7ebdbaa362f23646ecb476fc80fc79496bf
Dich <brcefy@gmail.com>
2025-05-31 18:05:55 +0800

    update:floorp

--- home/gui/browser/floorp.nix
+++ home/gui/browser/floorp.nix
@@ -20,31 +20,6 @@ _: {
           Locked = true;
           Value = true;
         };
-        ExtensionSettings = {
-          "*" = {
-            installation_mode = "blocked"; # Blocks all addons except the ones specified below
-          };
-          # Privacy Badger
-          "jid1-MnnxcxisBPnSXQ@jetpack" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/privacy-badger17/latest.xpi";
-            installation_mode = "force_installed";
-          };
-          # uBlock Origin
-          "uBlock0@raymondhill.net" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-            installation_mode = "force_installed";
-          };
-          # User-Agent Switcher and Manager
-          "{a6c4a591-f1b2-4f03-b3ff-767e5bedf4e7}" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/user-agent-string-switcher/latest.xpi";
-            installation_mode = "force_installed";
-          };
-          # Violentmonkey
-          "{aecec67f-0d10-4fa7-b7c7-609a2db280cf}" = {
-            install_url = "https://addons.mozilla.org/firefox/downloads/latest/violentmonkey/latest.xpi";
-            installation_mode = "force_installed";
-          };
-        };
         OverrideFirstRunPage = "";
         OverridePostUpdatePage = "";
         Preferences = {
