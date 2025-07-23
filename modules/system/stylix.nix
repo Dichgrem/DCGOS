c1ea3bf15f023786fdd6433654bbf818aa35d09f
dichgrem <brcefy@gmail.com>
2025-07-23 11:54:11 +0800

    chore:add_wallpaper

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -5,25 +5,25 @@
 }: {
   stylix = {
     enable = true;
-    # catppuccin-mocha base16Scheme # See https://github.com/catppuccin/base16
-    #      base16Scheme = {
-    #        base00 = "1e1e2e"; # base
-    #        base01 = "181825"; # mantle
-    #        base02 = "313244"; # surface0
-    #        base03 = "45475a"; # surface1
-    #        base04 = "585b70"; # surface2
-    #        base05 = "cdd6f4"; # text
-    #        base06 = "f5e0dc"; # rosewater
-    #        base07 = "b4befe"; # lavender
-    #        base08 = "f38ba8"; # red
-    #        base09 = "fab387"; # peach
-    #        base0A = "f9e2af"; # yellow
-    #        base0B = "a6e3a1"; # green
-    #        base0C = "94e2d5"; # teal
-    #        base0D = "89b4fa"; # blue
-    #        base0E = "cba6f7"; # mauve
-    #        base0F = "f2cdcd"; # flamingo
-    #      };
+#     catppuccin-mocha base16Scheme # See https://github.com/catppuccin/base16
+          base16Scheme = {
+            base00 = "1e1e2e"; # base
+            base01 = "181825"; # mantle
+            base02 = "313244"; # surface0
+            base03 = "45475a"; # surface1
+            base04 = "585b70"; # surface2
+            base05 = "cdd6f4"; # text
+            base06 = "f5e0dc"; # rosewater
+            base07 = "b4befe"; # lavender
+            base08 = "f38ba8"; # red
+            base09 = "fab387"; # peach
+            base0A = "f9e2af"; # yellow
+            base0B = "a6e3a1"; # green
+            base0C = "94e2d5"; # teal
+            base0D = "89b4fa"; # blue
+            base0E = "cba6f7"; # mauve
+            base0F = "f2cdcd"; # flamingo
+          };
     cursor = {
       name = "Bibata-Modern-Ice";
       package = pkgs.bibata-cursors;
@@ -46,7 +46,8 @@
         terminal = 12;
       };
     };
-    image = ../../dotfiles/.local/share/wallpapers/nixos-1.png;
+#    image = ../../dotfiles/.local/share/wallpapers/yohoho.jpg;
+#    image = null;
     polarity = "dark";
   };
 }
