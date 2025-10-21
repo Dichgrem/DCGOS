a32a68a24b58aae536ac6d0f1e3369c4aa4b733c
dichgrem <brcefy@gmail.com>
2025-10-21 10:59:44 +0800

    chore:add_diffview

--- home/nvim/lua/plugins/colorscheme.lua
+++ home/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,16 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      style = "storm",
-    },
-    config = function(_, opts)
-      require("tokyonight").setup(opts)
-      vim.defer_fn(function()
-        vim.cmd([[colorscheme tokyonight]])
-      end, 0)
-    end,
-  },
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			style = "storm",
+		},
+		config = function(_, opts)
+			require("tokyonight").setup(opts)
+			vim.defer_fn(function()
+				vim.cmd([[colorscheme tokyonight]])
+			end, 0)
+		end,
+	},
 }
