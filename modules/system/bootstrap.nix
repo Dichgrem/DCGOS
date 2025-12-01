21201ec897c01a719a82f8d7bc5e3e52fbcc5bf2
dichgrem <brcefy@gmail.com>
2025-12-01 16:29:24 +0800

    refactor:disable_watchdog

--- modules/system/bootstrap.nix
+++ modules/system/bootstrap.nix
@@ -17,9 +17,19 @@
 in
   with lib; {
     boot = {
-      bcache.enable = false;
+      blacklistedKernelModules = [
+        "iTCO_wdt"
+        "iTCO_vendor_support"
+        "intel_pmc_bxt"
+        "mei"
+        "mei_hdcp"
+        "mei_me"
+        "mei_pxp"
+        "pstore"
+        "sp5100_tco"
+        "wdat_wdt"
+      ];
       consoleLogLevel = 2; # Only errors and warnings are displayed
-      extraModprobeConfig = "blacklist mei mei_hdcp mei_me mei_pxp iTCO_wdt pstore sp5100_tco";
       extraModulePackages = [
         config.boot.kernelPackages.v4l2loopback # v4l2loopback is for OBS Virtual Cam Support
       ];
