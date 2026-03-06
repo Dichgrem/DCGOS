4188e32f25f17481c4ec76700c0ae2392cc36896
dichgrem <brcefy@gmail.com>
2026-03-06 19:19:39 +0800

    build:change_kernel

--- modules/services/qemu.nix
+++ modules/services/qemu.nix
@@ -1,17 +1,26 @@
-{
-  hostname,
-  lib,
-  ...
+{ hostname
+, lib
+, pkgs
+, ...
 }:
 with lib; let
   inherit (import ../../hosts/${hostname}/env.nix) QEMU-VM-Use-Case;
-in {
+in
+{
   config = mkIf QEMU-VM-Use-Case {
     services = {
       spice-vdagentd.enable = true;
       spice-webdavd.enable = true;
     };
     virtualisation.libvirtd.enable = true;
-    systemd.services.virt-secret-init-encryption.enable = false;
+
+    systemd.services.virt-secret-init-encryption = {
+      serviceConfig = {
+        ExecStart = [
+          ""
+          "${pkgs.bash}/bin/sh -c 'umask 0077 && (dd if=/dev/random status=none bs=32 count=1 | systemd-creds encrypt --name=secrets-encryption-key - /var/lib/libvirt/secrets/secrets-encryption-key)'"
+        ];
+      };
+    };
   };
 }
