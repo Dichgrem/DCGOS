e36483ed7c6ba9724032e94b8d313e4bc957c44d
dichgrem <brcefy@gmail.com>
2026-03-22 20:37:53 +0800

    update:flake
    update:swaybg
    
    chore:syncthing

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -2,7 +2,6 @@
   services.syncthing = {
     enable = true;
     user = username;
-    dataDir = "/home/${username}/Sync";
     configDir = "/home/${username}/.config/syncthing";
     guiAddress = "127.0.0.1:8384";
   };
