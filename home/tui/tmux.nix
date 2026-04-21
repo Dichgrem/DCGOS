1ca28d156be55b647670e041a44509589c7995ba
dichgrem <brcefy@gmail.com>
2026-04-21 20:49:09 +0800

    style:some
    style:btop
    style:tmux
    style:waybar
    style:swaync
    style:fuzzel
    style:hyprlock
    style:fastfetch

--- home/tui/tmux.nix
+++ home/tui/tmux.nix
@@ -1,32 +1,40 @@
 {pkgs, ...}: {
   programs.tmux = {
     enable = true;
-
     mouse = true;
     prefix = "C-a";
     historyLimit = 10000;
     terminal = "tmux-256color";
 
-    plugins = with pkgs.tmuxPlugins; [
-      sensible
-      resurrect
-      continuum
-      yank
-      catppuccin
-    ];
-
     extraConfig = ''
       set -g xterm-keys on
       set -s extended-keys on
-      set -g @catppuccin_flavour 'mocha'
-      set -g @catppuccin_window_tabs_enabled on
-      set -g @catppuccin_status_background "default"
-
+      set -g @catppuccin_flavor "mocha"
+      set -g @catppuccin_window_status_style "rounded"
       bind h select-pane -L
       bind j select-pane -D
       bind k select-pane -U
       bind l select-pane -R
       bind y set-window-option synchronize-panes
     '';
+
+    plugins = with pkgs.tmuxPlugins; [
+      sensible
+      yank
+      resurrect
+      continuum
+      catppuccin
+      {
+        plugin = pkgs.tmuxPlugins.catppuccin;
+        extraConfig = ''
+          set -g status-left-length 100
+          set -g status-right-length 100
+          set -g status-left ""
+          set -g status-right "#{E:@catppuccin_status_session}"
+          set -ag status-right "#{E:@catppuccin_status_application}"
+          set -ag status-right "#{E:@catppuccin_status_uptime}"
+        '';
+      }
+    ];
   };
 }
